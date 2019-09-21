6cba802d68e8e9b6693246937716745d08869b34
notamanbag <amanbagrecha@outlook.com>
2019-09-21 07:12:34 +0530

    Carbon Bhai

--- lib/carbon_screen.dart
+++ lib/carbon_screen.dart
@@ -1,99 +1,170 @@
 import 'package:flutter/material.dart';
 
+
 class CarbonFootprint extends StatefulWidget {
+  
   @override
   _CarbonFootprintState createState() => _CarbonFootprintState();
 }
 
 class _CarbonFootprintState extends State<CarbonFootprint> {
-  int _selectedIndex = 0;
-  String electricity,distance;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: TIPS ',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: NEWS',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: CALCULATOR',
-      style: optionStyle,
-    ),
-  ];
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  
+  static String electricity,distance,fuel,liqour;
+  //  int ele = int.parse(electricity);
+  //  int dis = int.parse(distance);
+  //   int  result= ele*dis;
+
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: Center(
-        child:Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(border: InputBorder.none,
-                hintText: 'Enter the use of electicty in kWh'),
-                
-                style: TextStyle(color: Colors.black),
-                textAlign: TextAlign.center,
-                
-                onChanged: (value) {
-                  electricity = value;
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(border: InputBorder.none,
-                hintText: 'Enter the approx distance travelled overall'),
-                
-                style: TextStyle(color: Colors.black),
-                textAlign: TextAlign.center,
-                
-                onChanged: (value) {
-                  distance = value;
-                },
-              ),
-
-           ],
-        )
-      ),
-
+        child:SingleChildScrollView(
+                  child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+             
+             children: <Widget>[
+               Image.asset('assets/images/Orbis.png',height: 150.0,),
+               SizedBox(
+                  height: 20.0,
+                ),
+              
+               
+               
+                TextField(
                   
+                  keyboardType: TextInputType.number,
+                   decoration: InputDecoration(
+                  hintText: 'Enter the electricity consumed in kWh',
+                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                      enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+                  ),
+                  
+                  style: TextStyle(color: Colors.black),
+                   textAlign: TextAlign.center,
+                    onChanged: (value) {
+                    electricity = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                  hintText: 'Enter the approx distance travelled overall',
+                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                      enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+                  ),
+                  
+                  style: TextStyle(color: Colors.black),
+                  textAlign: TextAlign.center,
+                  
+                  onChanged: (value) {
+                    distance = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  
+                  keyboardType: TextInputType.number,
+                   decoration: InputDecoration(
+                  hintText: 'Enter the fuel consumed in litres',
+                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                      enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+                  ),
+                  
+                  style: TextStyle(color: Colors.black),
+                   textAlign: TextAlign.center,
+                    onChanged: (value) {
+                    fuel = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  
+                  keyboardType: TextInputType.number,
+                   decoration: InputDecoration(
+                  hintText: 'Enter the intake of liquour in litres',
+                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                      enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+                  ),
+                  
+                  style: TextStyle(color: Colors.black),
+                   textAlign: TextAlign.center,
+                    onChanged: (value) {
+                    fuel = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RaisedButton(
+                  color: Colors.greenAccent,
+                  child: Text("Calculate"),
+                  
+                  onPressed: (){
+                    setState(() {
 
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.home),
-          //   title: Text('Orbis',style: TextStyle(color: Colors.black)),
-          // ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.warning),
-            title: Text('Tips',style: TextStyle(color: Colors.black)),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.burst_mode),
-            title: Text('News',style: TextStyle(color: Colors.black)),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dialpad),
-            title: Text('Carbon-Footprint',style: TextStyle(color: Colors.black),),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+                      
+                    });
+                  },
+                  
+                  
+                ),
+                 
+                
+             ],
+             
       ),
-
+        ),
+      ),
     );
   }
 }
